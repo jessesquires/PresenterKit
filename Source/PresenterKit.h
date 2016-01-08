11bbb20bfa46a1521ec43955154ff6dfadbaa4f6
Jesse Squires <jesse.d.squires@gmail.com>
2016-01-07 20:50:18 -0800

    add empty project

---
+++ Source/PresenterKit.h
@@ -0,0 +1,25 @@
+//
+//  Created by Jesse Squires
+//  http://www.jessesquires.com
+//
+//
+//  Documentation
+//  http://www.jessesquires.com/PresenterKit
+//
+//
+//  GitHub
+//  https://github.com/jessesquires/PresenterKit
+//
+//
+//  License
+//  Copyright © 2016-Present Jesse Squires
+//  Released under an MIT license: http://opensource.org/licenses/MIT
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for PresenterKit.
+FOUNDATION_EXPORT double PresenterKitVersionNumber;
+
+//! Project version string for PresenterKit.
+FOUNDATION_EXPORT const unsigned char PresenterKitVersionString[];
---
+++ Tests/PresenterKitTests.swift
@@ -0,0 +1,32 @@
+//
+//  Created by Jesse Squires
+//  http://www.jessesquires.com
+//
+//
+//  Documentation
+//  http://www.jessesquires.com/PresenterKit
+//
+//
+//  GitHub
+//  https://github.com/jessesquires/PresenterKit
+//
+//
+//  License
+//  Copyright © 2016-Present Jesse Squires
+//  Released under an MIT license: http://opensource.org/licenses/MIT
+//
+
+import XCTest
+@testable import PresenterKit
+
+final class PresenterKitTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+    }
+    
+    override func tearDown() {
+        super.tearDown()
+    }
+    
+}
