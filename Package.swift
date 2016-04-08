fe13f1ad59dee757f10827ad2d0cfdb2c75f58f3
Jesse Squires <jesse.d.squires@gmail.com>
2016-04-07 18:54:36 -0700

    support SPM

---
+++ Package.swift
@@ -0,0 +1,23 @@
+//
+//  Created by Jesse Squires
+//  http://www.jessesquires.com
+//
+//
+//  Documentation
+//  http://www.jessesquires.com/PresenterKit
+//
+//
+//  GitHub
+//  https://github.com/jessesquires/PresenterKit
+//
+//
+//  License
+//  Copyright © 2016-present Jesse Squires
+//  Released under an MIT license: http://opensource.org/licenses/MIT
+//
+
+import PackageDescription
+
+let package = Package(
+    name: "PresenterKit"
+)
