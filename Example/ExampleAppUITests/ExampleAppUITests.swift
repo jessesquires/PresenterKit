2b1b0622b2257c4b427f0693cebc827d13261306
Jesse Squires <jesse.d.squires@gmail.com>
2016-04-07 22:43:30 -0700

    add UI tests

--- Example/ExampleAppUITests/ExampleAppUITests.swift
+++ Example/ExampleAppUITests/ExampleAppUITests.swift
@@ -31,10 +31,46 @@ final class ExampleAppUITests: XCTestCase {
     override func tearDown() {
         super.tearDown()
     }
-    
-    func testExample() {
-        // Use recording to get started writing UI tests.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
+
+    func test_popover() {
+        let presenterkitNavigationBar = XCUIApplication().navigationBars["PresenterKit"]
+        presenterkitNavigationBar.buttons["Popover"].tap()
+        presenterkitNavigationBar.staticTexts["PresenterKit"].tap()
     }
     
+    func test_push() {
+        let app = XCUIApplication()
+        app.tables.staticTexts["Push"].tap()
+        app.navigationBars["Red View"].buttons["PresenterKit"].tap()
+    }
+
+    func test_modal() {
+        let app = XCUIApplication()
+        app.tables.staticTexts["Modal"].tap()
+        app.navigationBars["Red View"].buttons["Cancel"].tap()
+    }
+
+    func test_show() {
+        let app = XCUIApplication()
+        app.tables.staticTexts["Show"].tap()
+        app.navigationBars["Red View"].buttons["PresenterKit"].tap()
+    }
+
+    func test_showDetail() {
+        let app = XCUIApplication()
+        app.tables.staticTexts["ShowDetail"].tap()
+        app.navigationBars["Red View"].buttons["Cancel"].tap()
+    }
+
+    func test_popoverFromView() {
+        let app = XCUIApplication()
+        app.tables.staticTexts["PopoverFromView"].tap()
+        app.navigationBars["PresenterKit"].staticTexts["PresenterKit"].tap()
+    }
+
+    func test_custom() {
+        let app = XCUIApplication()
+        app.tables.staticTexts["Custom (\"Half Modal\")"].tap()
+        app.navigationBars["PresenterKit"].staticTexts["PresenterKit"].tap()
+    }
 }
