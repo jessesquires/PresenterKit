b772770dc2d0a13c585e82ab5afcfc7e93892fb9
Jesse Squires <jesse.d.squires@gmail.com>
2016-04-07 20:18:22 -0700

    more tests. #5

--- Tests/PresenterKitTests.swift
+++ Tests/PresenterKitTests.swift
@@ -77,6 +77,58 @@ final class PresenterKitTests: XCTestCase {
         XCTAssertEqual(item.action, action)
     }
 
+    func test_thatViewController_appliesStyle_withNavigation() {
+        let controller = UIViewController()
+        let nav = controller.withNavigationStyle(.withNavigation) as! UINavigationController
+        XCTAssertEqual(controller, nav.topViewController)
+    }
+
+    func test_thatViewController_appliesStyle_withNavigation_none() {
+        let controller = UIViewController()
+        let other = controller.withNavigationStyle(.none)
+        XCTAssertEqual(controller, other)
+    }
+
+    func test_thatViewController_appliesStyle_withPresentation() {
+        let controller = UIViewController()
+        let other = controller.withPresentation(.FormSheet)
+        XCTAssertEqual(controller, other)
+        XCTAssertEqual(controller.modalPresentationStyle, UIModalPresentationStyle.FormSheet)
+        XCTAssertEqual(other.modalPresentationStyle, UIModalPresentationStyle.FormSheet)
+
+        let controller2 = UIViewController()
+        let other2 = controller2.withPresentation(.PageSheet)
+        XCTAssertEqual(controller2, other2)
+        XCTAssertEqual(controller2.modalPresentationStyle, UIModalPresentationStyle.PageSheet)
+        XCTAssertEqual(other2.modalPresentationStyle, UIModalPresentationStyle.PageSheet)
+
+        let controller3 = UIViewController()
+        let other3 = controller3.withPresentation(.Custom)
+        XCTAssertEqual(controller3, other3)
+        XCTAssertEqual(controller3.modalPresentationStyle, UIModalPresentationStyle.Custom)
+        XCTAssertEqual(other3.modalPresentationStyle, UIModalPresentationStyle.Custom)
+    }
+
+    func test_thatViewController_appliesStyle_withTransition() {
+        let controller = UIViewController()
+        let other = controller.withTransition(.CoverVertical)
+        XCTAssertEqual(controller, other)
+        XCTAssertEqual(controller.modalTransitionStyle, UIModalTransitionStyle.CoverVertical)
+        XCTAssertEqual(other.modalTransitionStyle, UIModalTransitionStyle.CoverVertical)
+
+        let controller2 = UIViewController()
+        let other2 = controller2.withTransition(.CrossDissolve)
+        XCTAssertEqual(controller2, other2)
+        XCTAssertEqual(controller2.modalTransitionStyle, UIModalTransitionStyle.CrossDissolve)
+        XCTAssertEqual(other2.modalTransitionStyle, UIModalTransitionStyle.CrossDissolve)
+
+        let controller3 = UIViewController()
+        let other3 = controller3.withTransition(.FlipHorizontal)
+        XCTAssertEqual(controller3, other3)
+        XCTAssertEqual(controller3.modalTransitionStyle, UIModalTransitionStyle.FlipHorizontal)
+        XCTAssertEqual(other3.modalTransitionStyle, UIModalTransitionStyle.FlipHorizontal)
+    }
+    
 
     // MARK: Helpers
 
