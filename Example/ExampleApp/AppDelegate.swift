77edb02cb197fb6b1db2a5de77f4ff778c72157e
Jesse Squires <jesse.d.squires@gmail.com>
2016-08-16 08:53:50 -0700

    update for Xcode8 beta6

--- Example/ExampleApp/AppDelegate.swift
+++ Example/ExampleApp/AppDelegate.swift
@@ -23,7 +23,8 @@ final class AppDelegate: UIResponder, UIApplicationDelegate {
 
     var window: UIWindow?
 
-    func application(_ application: UIApplication, didFinishLaunchingWithOptions options: [NSObject: AnyObject]?) -> Bool {
+    func application(_ application: UIApplication,
+                     willFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey : Any]? = nil) -> Bool {
         return true
     }
 }
